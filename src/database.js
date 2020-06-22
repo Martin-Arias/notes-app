e885449381067accc3d65fb5bc29e7df01dfbb47
Martin-Arias <arias95martin@gmail.com>
2020-06-22 16:46:06 -0300

    Minor fixes

--- src/database.js
+++ src/database.js
@@ -2,12 +2,13 @@
 
 const mongoose = require('mongoose');
 
-const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
 
-const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+// const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE} = process.env;
+
+// const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
 
 //
-mongoose.connect(MONGODB_URI,{
+mongoose.connect(process.env.MONGODB_URI,{
     useUnifiedTopology: true , 
     useNewUrlParser: true,
     useCreateIndex:true
